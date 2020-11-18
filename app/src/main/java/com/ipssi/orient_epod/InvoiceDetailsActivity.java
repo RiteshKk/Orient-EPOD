9be1c81e31016b3649cebab7d4a69e39ed3ac2f6
riteshKk <ritesh.chaurasia04@gmail.com>
2020-11-18 16:47:05 +0530

    only top 3 receiver will be counted

--- app/src/main/java/com/ipssi/orient_epod/InvoiceDetailsActivity.java
+++ app/src/main/java/com/ipssi/orient_epod/InvoiceDetailsActivity.java
@@ -51,15 +51,15 @@ public class InvoiceDetailsActivity extends AppCompatActivity {
         binding.setLifecycleOwner(this);
         Invoice model = (Invoice) getIntent().getParcelableExtra(AppConstant.MODEL);
         StringBuilder address = new StringBuilder();
-        if(model.getShiptopartyAddress()!=null || !model.getShiptopartyAddress().isEmpty()){
+        if (model.getShiptopartyAddress() != null && !model.getShiptopartyAddress().isEmpty()) {
             address.append(model.getShiptopartyAddress());
         }
-        if(model.getShiptopartyAddress1()!=null || !model.getShiptopartyAddress1().isEmpty()){
-            address.append(address.length()>0?"\n":"");
+        if (model.getShiptopartyAddress1() != null && !model.getShiptopartyAddress1().isEmpty()) {
+            address.append(address.length() > 0 ? "\n" : "");
             address.append(model.getShiptopartyAddress1());
         }
-        if(model.getShiptopartyAddress2()!=null || !model.getShiptopartyAddress2().isEmpty()){
-            address.append(address.length()>0?"\n":"");
+        if (model.getShiptopartyAddress2() != null && !model.getShiptopartyAddress2().isEmpty()) {
+            address.append(address.length() > 0 ? "\n" : "");
             address.append(model.getShiptopartyAddress2());
         }
         model.setShiptopartyAddress(address.toString());
@@ -97,12 +97,15 @@ public class InvoiceDetailsActivity extends AppCompatActivity {
                     ArrayList<Receiver> data = receivers.getData();
                     Invoice model = (Invoice) getIntent().getParcelableExtra(AppConstant.MODEL);
                     ArrayList<PlaceholderFragment> fragments = new ArrayList<>();
-                    for (Receiver receiverModel : data) {
-                        totalDamage += Integer.parseInt(receiverModel.getShortage());
+                    for (int i = 0; data != null && i < data.size(); i++) {
+                        if (i < 3) {
+                            Receiver receiverModel = data.get(i);
+                            totalDamage += Integer.parseInt(receiverModel.getShortage());
+                        }
                     }
-                    fragments.add(PlaceholderFragment.newInstance(model, data.size() > 0 ? data.get(0) : null));
-                    fragments.add(PlaceholderFragment.newInstance(model, data.size() > 1 ? data.get(1) : null));
-                    fragments.add(PlaceholderFragment.newInstance(model, data.size() > 2 ? data.get(2) : null));
+                    fragments.add(PlaceholderFragment.newInstance(model, (data != null && data.size() > 0) ? data.get(0) : null));
+                    fragments.add(PlaceholderFragment.newInstance(model, (data != null && data.size() > 1) ? data.get(1) : null));
+                    fragments.add(PlaceholderFragment.newInstance(model, (data != null && data.size() > 2) ? data.get(2) : null));
                     SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), fragments);
                     binding.viewPager.setAdapter(sectionsPagerAdapter);
                     binding.tabLayout.setupWithViewPager(binding.viewPager);
@@ -139,10 +142,8 @@ public class InvoiceDetailsActivity extends AppCompatActivity {
             finish();
         } else if (item.getItemId() == R.id.menu_change_language) {
             Intent intent = new Intent(this, LanguageSelectorActivity.class);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
