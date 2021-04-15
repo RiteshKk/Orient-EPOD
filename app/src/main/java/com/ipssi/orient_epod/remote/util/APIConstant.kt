e8f6e40263ccd20950fbef1b413a28ee34fd390b
riteshKk <ritesh.chaurasia04@gmail.com>
2021-04-16 05:21:05 +0530

    IP address updated

--- app/src/main/java/com/ipssi/orient_epod/remote/util/APIConstant.kt
+++ app/src/main/java/com/ipssi/orient_epod/remote/util/APIConstant.kt
@@ -1,6 +1,7 @@
 package com.ipssi.orient_epod.remote.util
 
-const val BASE_URL = "http://182.72.88.12:4000/"
+const val BASE_URL = "http://61.0.231.147:4000/"
+//const val BASE_URL = "http://182.72.88.12:4000/"
 const val SEARCH_DRIVER_SHIPMENT = "searchDriverShipments"
 const val SAVE_RECEIVER = "saveReceiver"
 const val GET_RECEIVERS = "getInvoiceReceivers/{invoice}"
